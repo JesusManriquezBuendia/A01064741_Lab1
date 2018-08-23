b7c7b4f71b13abe3d4b4700cbe57d653ae5e1107
Jesus Buendia <jesusbuendia13@gmail.com>
2018-08-23 14:31:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jesus_pc/a01064741_lab1/A01064741_Lab1.java
@@ -0,0 +1,13 @@
+package com.example.jesus_pc.a01064741_lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class A01064741_Lab1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a01064741__lab1);
+    }
+}
